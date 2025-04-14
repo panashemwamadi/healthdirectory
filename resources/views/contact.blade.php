79fa0e699953e0b3821e7061012e820c3ee0f22c
panashemwamadi <pmwthec@gmail.com>
2025-04-14 14:57:03 +0200

    Update contact.blade.php

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     
-        <section class="h-96 bg-blue-950 dark:bg-gray-900 bg-center bg-cover" style="background-image: url(images/pantech.png)">
+        <section class="h-96 bg-blue-950 dark:bg-gray-900 bg-center bg-cover" style="background-image: url(images/Glowbanner.jpg)">
         </div>
     
         <main class="m-2">

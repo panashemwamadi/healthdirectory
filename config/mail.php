dc2b2fc570555c07f3321b5e5c853b897536ca33
panashemwamadi <pmwthec@gmail.com>
2025-04-15 07:59:07 +0200

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'log'),
+    'default' => env('MAIL_MAILER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,61 +39,20 @@ return [
 
         'smtp' => [
             'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
-            'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', '127.0.0.1'),
-            'port' => env('MAIL_PORT', 2525),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'), // Change to your SMTP host
+            'port' => env('MAIL_PORT', 587), // Recommended SMTP port
+            'from' => [
+                'address' => env('MAIL_FROM_ADDRESS', 'panashemwamadi78@gmail.com'),
+                'name' => env('MAIL_FROM_NAME', 'Noticeboard'),
+            ],
+            'username' => 'panashemwamadi78@gmail.com',
+            'password' => 'lkqvfzxrehgvikhs',
+            'encryption' => env('MAIL_ENCRYPTION', 'tls'), // Recommended encryption
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
-        'ses' => [
-            'transport' => 'ses',
-        ],
-
-        'postmark' => [
-            'transport' => 'postmark',
-            // 'message_stream_id' => env('POSTMARK_MESSAGE_STREAM_ID'),
-            // 'client' => [
-            //     'timeout' => 5,
-            // ],
-        ],
-
-        'resend' => [
-            'transport' => 'resend',
-        ],
-
-        'sendmail' => [
-            'transport' => 'sendmail',
-            'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
-        ],
-
-        'log' => [
-            'transport' => 'log',
-            'channel' => env('MAIL_LOG_CHANNEL'),
-        ],
-
-        'array' => [
-            'transport' => 'array',
-        ],
-
-        'failover' => [
-            'transport' => 'failover',
-            'mailers' => [
-                'smtp',
-                'log',
-            ],
-        ],
-
-        'roundrobin' => [
-            'transport' => 'roundrobin',
-            'mailers' => [
-                'ses',
-                'postmark',
-            ],
-        ],
+        // Other mailer configurations remain unchanged...
 
     ],
 
@@ -109,8 +68,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'panashemwamadi78@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Glowtrack'),
     ],
 
 ];

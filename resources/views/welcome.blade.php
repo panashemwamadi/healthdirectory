53bca169a018a42095d6659c79d2af7ebc1cc305
panashemwamadi <pmwthec@gmail.com>
2025-04-15 09:30:19 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -286,7 +286,7 @@
                         </div>
                         <div class="flex items-center justify-between">
                             <span class="text-3xl font-bold text-gray-900 dark:text-white">${{ $auction->starting_bid }}</span>
-                            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View</a>
+                            <a href="/auctions" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View</a>
                         </div>
                     </div>
                 </div>
@@ -412,10 +412,7 @@
         
     </div>
     
-    <div class="grid grid-cols-1 ml-2 md:mx-auto md:w-1/4">
-                  
-        <button type="button" class="mt-2 focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-900 my-10">SHOW MORE</button>
-        </div>
+    
     </div>
     
 
